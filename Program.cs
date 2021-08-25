54117ce649008cf991da6e4f4f4b6ae1ef03f95f
bbrb@ukr.net <bbrb@ukr.net>
2021-08-25 16:50:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=UsersDB.sqlite";
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "SELECT * FROM users";
+
+            SqliteCommand command = new SqliteCommand(query, connection);
+
+            SqliteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                //dataGridView1.DataSource
+                string id = reader.GetString(0);
+
+                Console.WriteLine(id);
+            }
+        }
+    }
+}
